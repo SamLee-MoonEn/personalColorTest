786812ec7d7da23977fc0669e1a9065a0b539ad8
SamLee <goha2010@naver.com>
2023-04-10 22:41:31 +0900

    component 분리 및  main page 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Nav from './component/Nav';
 import Sidebar from './component/Sidebar';
+import MainPage from './Pages/MainPage';
 
 export default function App() {
   return (
@@ -10,7 +11,7 @@ export default function App() {
           <div className=" drawer-content">
             <div className=" mockup-window border-2 border-gray-600 bg-gray-400">
               <Nav />
-              <div className=" flex justify-center items-center text-3xl bg-white border-l border-r border-b border-solid border-base-300 h-48">컨텐츠</div>
+              <MainPage></MainPage>
             </div>
           </div>
           <Sidebar />
---
+++ src/Pages/MainPage.tsx
@@ -0,0 +1,14 @@
+export default function MainPage() {
+  return (
+    <section className="wrapper h-96 justify-evenly">
+      <div className=" text-center">
+        <h1 className="text-lg md:text-2xl">인공지능 Personal Color 테스트</h1>
+        <h2 className="text-sm md:text-lg">나의 Personal Color는 무엇일까?</h2>
+      </div>
+      <div className="flex flex-col items-center">
+        <input type="file" className="file-input file-input-bordered file-input-primary w-10/12 md:w-full max-w-xs mb-5" />
+        <h3 className="text-gray-500 text-xs md:text-md">얼굴 사진을 올려놓거나 눌러서 업로드하세요!</h3>
+      </div>
+    </section>
+  );
+}
